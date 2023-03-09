d131c45c63d51dabfc9d3e754d5a06f0dd28a48b
NoteAP00 <apinun893@gmail.com>
2023-03-09 21:42:59 +0700

    modified:   toc.php

--- toc.php
+++ toc.php
@@ -6,7 +6,7 @@ $toc = "<h2>Table of Contents</h2>\n<ul>\n";
 foreach ($files as $file) {
   if ($file != "." && $file != ".." && strpos($file, ".html") !== false) {
     // Add the file name to the Table of Contents
-    $toc .= "<li><a href=\"$file\">$file</a></li>\n";
+    $toc .= "<li><a href=\"$file\" target=\"_blank\">$file</a></li>\n";
   }
 }
 
