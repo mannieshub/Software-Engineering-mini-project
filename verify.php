2e8468cc03883d08b0eee7b48a797dd3a2d40a3e
NoteAP00 <apinun893@gmail.com>
2023-03-19 14:35:43 +0700

    Fix : Verify page no secure

--- verify.php
+++ verify.php
@@ -14,23 +14,28 @@ $conn = new PDO("mysql:host=$host;port=$dbport;dbname=$dbname", $dbusername, $db
 
   isset($_POST['email']) ? $u = $_POST['email'] : $u = "";
   isset($_POST['pwd']) ? $p = $_POST['pwd'] : $p = "";
-  $shapass = sha1($p);
-  $query = "SELECT * FROM studentDB WHERE Email='{$u}' AND Password='{$shapass}'";
-  $result = $conn->query($query);
-  if($result->rowCount()==1){
-    // Valid login credentials
-    $data=$result->fetch(PDO::FETCH_ASSOC);
-    $_SESSION['stdid'] = $data["stdID"];
-    $_SESSION["username"] = $data["stdName"];
-    $_SESSION["email"] = $u;
-    $_SESSION["role"] = $data["Role"];
-    $_SESSION["id"] = session_id();
-    header("Location: homepage.php"); // Redirect to the home page
-    die();
+  if(isset($_POST['email']) && isset($_POST['pwd'])){
+    $shapass = sha1($p);
+    $query = "SELECT * FROM studentDB WHERE Email='{$u}' AND Password='{$shapass}'";
+    $result = $conn->query($query);
+    if($result->rowCount()==1){
+      // Valid login credentials
+      $data=$result->fetch(PDO::FETCH_ASSOC);
+      $_SESSION['stdid'] = $data["stdID"];
+      $_SESSION["username"] = $data["stdName"];
+      $_SESSION["email"] = $u;
+      $_SESSION["role"] = $data["Role"];
+      $_SESSION["id"] = session_id();
+      header("Location: homepage.php"); // Redirect to the home page
+      die();
+    }else{
+      $_SESSION["error"] = 1;
+      header("Location: login.php");       
+      die();
+  }
   }else{
-    $_SESSION["error"] = 1;
     header("Location: login.php");       
     die();
-}
+  }
+    
 $conn=null;
-?>
